764ebf0e4bcc7853c3fad06932929ec1844a903e
fresco <fresco@frescos-iMac.local>
2019-10-07 21:37:53 -0400

    MVP complete, ProductContext and CartContext created and working. might attempt stretch tonight

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import { ProductContext } from './context/ProductContext'
+import { CartContext } from './context/CartContext'
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -21,24 +25,19 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<ProductContext.Provider value={{products, addItem}}>
+				<CartContext.Provider value={cart}>
+					<Navigation />
+					{/* Routes */}				
+						<Route
+							exact
+							path="/"
+							component={Products} />			
+						<Route
+							path="/cart"
+							component={ShoppingCart} />
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
